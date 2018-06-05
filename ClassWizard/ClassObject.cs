83ffe6fdec18de04773c7282182e938fe7af454c
MaxiMHc <maximgt540@gmail.com>
2018-06-05 16:44:16 +0200

    Add ClassObject frame

--- ClassWizard/ClassObject.cs
+++ ClassWizard/ClassObject.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace ClassWizard
 {
-    class ClassObject
+    public class ClassObject
     {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<string> Keywords { get; set; }
+        public string Inheritance { get; set; }
+        public List<string> Interfaces { get; set; }
+        public List<PropertyObject> Properties { get; set; }
+        public List<MethodObject> Methods { get; set; }
+    }
 
+    public class MethodObject
+    {
+        public string Name { get; set; }
+        public string AccessModifier { get; set; }
+        public string ReturnType { get; set; }
+        public List<string> Keywords { get; set; }
+        public List<ArgumentObject> Arguments { get; set; }
+    }
+
+    public class ArgumentObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<string> Keywords { get; set; }
+    }
+
+    public class PropertyObject
+    {
+        public string Name { get; set; }
+        public string AccessModifier { get; set; }
+        public string Type { get; set; }
+        public List<string> Keywords { get; set; }
     }
 }
